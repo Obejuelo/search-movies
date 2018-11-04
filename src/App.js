fff619b30899950c3bb0c2d720bf22c7504ca040
Obed <obejuela4@gmail.com>
2018-11-04 11:14:20 -0600

    comments of routes

--- src/App.js
+++ src/App.js
@@ -13,21 +13,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
+      {/* Animations for pages */}
       <Route render={({location}) => (
         <TransitionGroup>
           <CSSTransition
 							key={location.key}
 							timeout={300}
 							classNames="fade">
+
+            {/* Routes and pages */}
             <Switch location={location}>
               <Route exact path='/' component={Home}/>
               <Route path='/detail/:id' component={Detail} />
               <Route component={NotFound} />
             </Switch>
+
           </CSSTransition>
         </TransitionGroup>
       )}>
       </Route>
+      
       </div>
     );
   }
